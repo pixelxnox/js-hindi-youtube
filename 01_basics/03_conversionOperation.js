dc24044453c74b8fe073ac0de335e848f2a284b5
pixelxnox <161120441+pixelxnox@users.noreply.github.com>
2024-12-15 15:54:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score ="33abb"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)              //cnversion
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN Not a number
+// true => 1, false => true
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false; "sharon" => true
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
+
+
