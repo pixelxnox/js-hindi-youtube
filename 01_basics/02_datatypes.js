68aa55acb7c1a1b56ad3e1469690baf4d7dd34ad
pixelxnox <161120441+pixelxnox@users.noreply.github.com>
2024-12-15 15:24:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";       //treat all JS code as newer version
+
+// alert(3+3)      //we are using nodejs ,not browser
+
+console.log(3
+    +3)              //code readability should be high
+
+console.log("Sharon")
+
+
+let name ="Sharon"
+let age = 21
+let isLoggedIn = true
+
+
+//number => 2 power 53
+//bigint
+//boolean => true or falsw
+//string => ""
+//null => standalone value
+//undefined => like that of state
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);   //undefined output
+console.log(typeof null);        //object
