8880104360977f445c082771e81cbf25a51aa2e5
pixelxnox <161120441+pixelxnox@users.noreply.github.com>
2024-12-21 13:55:05 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,8 +25,28 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let someNumber = 33
 
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+//**********************************operations************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2**2);    //raise to(2 raise to 2)
+
+let str1="hello"
+let str2=" sharon"
+
+console.log(str1 + str2);
+
+let gameCounter =100
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+
+
 
 
 
